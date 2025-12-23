ab9b6cad9725f9a370002ad1d5613a4c2322801f
Akshat Tiwari <tiwariar_10@rknec.edu>
2025-12-23 11:42:57 +0530

    Time: 0 ms (100.00%) | Memory: 46.7 MB (58.10%) - LeetSync

--- 141-linked-list-cycle/linked-list-cycle.java
+++ 141-linked-list-cycle/linked-list-cycle.java
@@ -11,16 +11,28 @@
  */
 public class Solution {
     public boolean hasCycle(ListNode head) {
-        HashSet<ListNode> set = new HashSet<>();
+        // HashSet<ListNode> set = new HashSet<>();
 
-        while(head != null) {
-            if(set.contains(head)){
+        // while(head != null) {
+        //     if(set.contains(head)){
+        //         return true;
+        //     }
+        //     set.add(head);
+        //     head = head.next;
+        // }
+
+        // return false;
+
+        ListNode slow=head;
+        ListNode fast=head;
+
+        while(fast!=null && fast.next!=null){
+            slow=slow.next;
+            fast=fast.next.next;
+            if(slow==fast){
                 return true;
             }
-            set.add(head);
-            head = head.next;
         }
-
         return false;
     }
 }
