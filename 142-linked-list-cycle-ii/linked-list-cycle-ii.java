1797ab323695eb6dca62bab74f3eb405f9f2b80c
Akshat Tiwari <tiwariar_10@rknec.edu>
2025-12-29 18:35:03 +0530

    Time: 6 ms (9.81%) | Memory: 46.5 MB (79.92%) - LeetSync

---
+++ 142-linked-list-cycle-ii/linked-list-cycle-ii.java
@@ -0,0 +1,18 @@
+import java.util.HashMap;
+
+public class Solution {
+    public ListNode detectCycle(ListNode head) {
+        HashMap<ListNode, Boolean> map = new HashMap<>();
+        ListNode temp = head;
+
+        while (temp != null) {
+            if (map.containsKey(temp)) {
+                return temp; 
+            }
+            map.put(temp, true);
+            temp = temp.next;
+        }
+
+        return null; 
+    }
+}
