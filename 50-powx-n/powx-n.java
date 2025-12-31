ef7391935cb32b08b85671d294d738e34151f1c0
Akshat Tiwari <tiwariar_10@rknec.edu>
2025-12-31 22:20:48 +0530

    Time: 0 ms (100.00%) | Memory: 47.7 MB (69.99%) - LeetSync

--- 50-powx-n/powx-n.java
+++ 50-powx-n/powx-n.java
@@ -1,5 +1,20 @@
 class Solution {
+    private double power(double x, long n){
+        if(n == 0) return 1;
+        if(n == 1) return x;
+        double half = power(x, n/2);
+            if(n % 2 == 0){
+                 return half * half;
+            }
+            else{
+                return half * half * x;
+            }
+    }
     public double myPow(double x, int n) {
-        return Math.pow(x, n);
+        long N = n;
+        if(n < 0){
+            return 1 / power(x, -1 * N);
+        }
+        return power(x, N);
     }
 }
