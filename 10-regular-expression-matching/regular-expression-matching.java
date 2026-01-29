bd57a14a7e27303e8eb870059ba76c4d614c5278
Akshat Tiwari <tiwariar_10@rknec.edu>
2026-01-29 19:31:32 +0530

    Time: 4 ms (28.63%) | Memory: 43.9 MB (25.46%) - LeetSync

---
+++ 10-regular-expression-matching/regular-expression-matching.java
@@ -0,0 +1,35 @@
+class Solution {
+    public boolean isMatch(String s, String p) {
+        int m = s.length();
+        int n = p.length();
+        boolean[][] dp = new boolean[m + 1][n + 1];
+        dp[0][0] = true;
+        
+        // Initialize for empty string matching patterns like "a*", "b*", etc.
+        for (int j = 1; j <= n; j++) {
+            if (p.charAt(j - 1) == '*') {
+                dp[0][j] = dp[0][j - 2];
+            }
+        }
+        
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (p.charAt(j - 1) == '*') {
+                    // Zero occurrences of the preceding element
+                    dp[i][j] = dp[i][j - 2];
+                    // One or more occurrences if it matches
+                    if (p.charAt(j - 2) == '.' || p.charAt(j - 2) == s.charAt(i - 1)) {
+                        dp[i][j] |= dp[i - 1][j];
+                    }
+                } else {
+                    // Normal character or '.'
+                    if (p.charAt(j - 1) == '.' || p.charAt(j - 1) == s.charAt(i - 1)) {
+                        dp[i][j] = dp[i - 1][j - 1];
+                    }
+                }
+            }
+        }
+        
+        return dp[m][n];
+    }
+}
