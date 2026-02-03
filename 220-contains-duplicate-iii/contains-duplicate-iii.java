df324ed7cec5b41782525880172b6897a327821c
Akshat Tiwari <tiwariar_10@rknec.edu>
2026-02-03 21:43:15 +0530

    Time: 62 ms (91.78%) | Memory: 126.4 MB (32.99%) - LeetSync

---
+++ 220-contains-duplicate-iii/contains-duplicate-iii.java
@@ -0,0 +1,40 @@
+import java.util.HashMap;
+import java.util.Map;
+
+class Solution {
+    public boolean containsNearbyAlmostDuplicate(int[] nums, int indexDiff, int valueDiff) {
+        if (nums == null || nums.length < 2 || indexDiff <= 0 || valueDiff < 0) {
+            return false;
+        }
+        Map<Long, Long> buckets = new HashMap<>();
+        long width = (long) valueDiff + 1;
+
+        for (int i = 0; i < nums.length; i++) {
+            long val = (long) nums[i];
+            long bucketId = getBucketId(val, width);
+
+            if (buckets.containsKey(bucketId)) {
+                return true;
+            }
+
+            if (buckets.containsKey(bucketId - 1) && Math.abs(val - buckets.get(bucketId - 1)) <= valueDiff) {
+                return true;
+            }
+            if (buckets.containsKey(bucketId + 1) && Math.abs(val - buckets.get(bucketId + 1)) <= valueDiff) {
+                return true;
+            }
+
+            buckets.put(bucketId, val);
+            if (i >= indexDiff) {
+                long lastBucketId = getBucketId((long) nums[i - indexDiff], width);
+                buckets.remove(lastBucketId);
+            }
+        }
+
+        return false;
+    }
+
+    private long getBucketId(long val, long width) {
+        return val < 0 ? (val + 1) / width - 1 : val / width;
+    }
+}
