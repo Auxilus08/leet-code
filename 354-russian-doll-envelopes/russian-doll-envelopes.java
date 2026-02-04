6d234d2f7caa45c2e84a0f49169d7349a9cdfab5
Akshat Tiwari <tiwariar_10@rknec.edu>
2026-02-04 22:05:48 +0530

    Time: 44 ms (50.92%) | Memory: 103.7 MB (15.09%) - LeetSync

---
+++ 354-russian-doll-envelopes/russian-doll-envelopes.java
@@ -0,0 +1,36 @@
+class Solution {
+    public int maxEnvelopes(int[][] envelopes) {
+        Arrays.sort(envelopes, (a, b) -> {
+            if (a[0] == b[0]) {
+                return b[1] - a[1];
+            } else {
+                return a[0] - b[0];
+            }
+        });
+
+        int[] heights = new int[envelopes.length];
+        for (int i = 0; i < envelopes.length; i++) {
+            heights[i] = envelopes[i][1];
+        }
+
+        return lengthOfLIS(heights);
+    }
+
+    private int lengthOfLIS(int[] nums) {
+        int[] tails = new int[nums.length];
+        int size = 0;
+        for (int x : nums) {
+            int i = 0, j = size;
+            while (i != j) {
+                int m = (i + j) / 2;
+                if (tails[m] < x)
+                    i = m + 1;
+                else
+                    j = m;
+            }
+            tails[i] = x;
+            if (i == size) size++;
+        }
+        return size;
+    }
+}
