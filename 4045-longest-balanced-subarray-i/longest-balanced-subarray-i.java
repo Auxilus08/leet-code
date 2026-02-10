fc221a2d1569ea9d19ba877ef0bf56d39a692059
Akshat Tiwari <tiwariar_10@rknec.edu>
2026-02-10 21:47:10 +0530

    Time: 85 ms (95.44%) | Memory: 47.2 MB (45.62%) - LeetSync

--- 4045-longest-balanced-subarray-i/longest-balanced-subarray-i.java
+++ 4045-longest-balanced-subarray-i/longest-balanced-subarray-i.java
@@ -3,24 +3,24 @@ class Solution {
         int n = nums.length;
         int maxLen = 0;
 
-        for (int i = 0; i < n; i++) {
-            if (n - i <= maxLen) break;
+        for(int i = 0; i < n; i++){
+            if(n - i <= maxLen) break;
 
-            java.util.HashSet<Integer> seen = new java.util.HashSet<>();
             int evenCount = 0;
             int oddCount = 0;
 
-            for (int j = i; j < n; j++) {
-                if (seen.add(nums[j])) {
-                    if (nums[j] % 2 == 0) evenCount++;
+            HashSet<Integer> seen = new HashSet<>();
+            for(int j = i; j < n; j++){
+                if(seen.add(nums[j])){
+                    if(nums[j] % 2 == 0) evenCount++;
                     else oddCount++;
                 }
 
-                if (evenCount == oddCount) {
-                    maxLen = Math.max(maxLen, j - i + 1);
-                }
+
+                if(evenCount == oddCount) maxLen = Math.max(maxLen, j - i + 1);
             }
         }
+
         return maxLen;
     }
 }
