2c1ea276c45b14aa7ad4fb858df242ab3a5b5416
Akshat Tiwari <tiwariar_10@rknec.edu>
2026-03-09 09:29:51 +0530

    Time: 6 ms (100.00%) | Memory: 45.6 MB (98.68%) - LeetSync

--- 3406-find-all-possible-stable-binary-arrays-i/find-all-possible-stable-binary-arrays-i.java
+++ 3406-find-all-possible-stable-binary-arrays-i/find-all-possible-stable-binary-arrays-i.java
@@ -1,48 +1,21 @@
 class Solution {
-    private final int MOD = (int) 1e9 + 7;
-  
-    private Long[][][] dp;
-  
-    private int maxConsecutive;
-
     public int numberOfStableArrays(int zero, int one, int limit) {
-        dp = new Long[zero + 1][one + 1][2];
-        this.maxConsecutive = limit;
-      
-        long result = (dfs(zero, one, 0) + dfs(zero, one, 1)) % MOD;
-        return (int) result;
-    }
+        final int mod = 1_000_000_007;
+        int L = limit + 1;
 
-    /**
-     * Dynamic programming with memoization to count stable arrays
-     * @param zerosLeft - number of zeros remaining to place
-     * @param onesLeft - number of ones remaining to place
-     * @param lastDigit - the last digit placed (0 or 1)
-     * @return number of valid stable arrays with given constraints
-     */
-    private long dfs(int zerosLeft, int onesLeft, int lastDigit) {
-        if (zerosLeft < 0 || onesLeft < 0) {
-            return 0;
+        int[][] dp0 = new int[zero + 1][one + 1]; 
+        int[][] dp1 = new int[zero + 1][one + 1];
+        for (int i = 1; i <= Math.min(zero, limit); ++i) dp0[i][0] = 1;
+        for (int j = 1; j <= Math.min(one, limit); ++j) dp1[0][j] = 1;
+        for (int i = 1; i <= zero; ++i) {
+            for (int j = 1; j <= one; ++j) {
+                dp0[i][j] = (dp0[i - 1][j] + dp1[i - 1][j] - (i >= L ? dp1[i - L][j] : 0)) % mod;
+                dp1[i][j] = (dp1[i][j - 1] + dp0[i][j - 1] - (j >= L ? dp0[i][j - L] : 0)) % mod;
+                dp0[i][j] = (dp0[i][j] + mod) % mod;
+                dp1[i][j] = (dp1[i][j] + mod) % mod;
+            }
         }
-        if (zerosLeft == 0) {
-            return (lastDigit == 1 && onesLeft <= maxConsecutive) ? 1 : 0;
-        }
-        if (onesLeft == 0) {
-            return (lastDigit == 0 && zerosLeft <= maxConsecutive) ? 1 : 0;
-        }
-        if (dp[zerosLeft][onesLeft][lastDigit] != null) {
-            return dp[zerosLeft][onesLeft][lastDigit];
-        }
-        if (lastDigit == 0) {
-            long totalArrays = dfs(zerosLeft - 1, onesLeft, 0) + dfs(zerosLeft - 1, onesLeft, 1);
-            long invalidArrays = dfs(zerosLeft - maxConsecutive - 1, onesLeft, 1);
-            dp[zerosLeft][onesLeft][lastDigit] = (totalArrays - invalidArrays + MOD) % MOD;
-        } else {
-            long totalArrays = dfs(zerosLeft, onesLeft - 1, 0) + dfs(zerosLeft, onesLeft - 1, 1);
-            long invalidArrays = dfs(zerosLeft, onesLeft - maxConsecutive - 1, 0);
-            dp[zerosLeft][onesLeft][lastDigit] = (totalArrays - invalidArrays + MOD) % MOD;
-        }
-      
-        return dp[zerosLeft][onesLeft][lastDigit];  
+
+        return (dp0[zero][one] + dp1[zero][one]) % mod;
     }
 }
