4ff40d81f7415317c3e6fb008e37a1a27e0f33c6
Akshat Tiwari <tiwariar_10@rknec.edu>
2026-05-27 10:09:30 +0530

    Time: 7 ms (100.00%) | Memory: 48.1 MB (48.35%) - LeetSync

--- 3405-count-the-number-of-special-characters-ii/count-the-number-of-special-characters-ii.java
+++ 3405-count-the-number-of-special-characters-ii/count-the-number-of-special-characters-ii.java
@@ -1,37 +1,24 @@
 class Solution {
     public int numberOfSpecialChars(String word) {
-
-        int[] lower = new int[26];
-        int[] upper = new int[26];
-
-        Arrays.fill(lower, -1);
-        Arrays.fill(upper, -1);
-
-        for (int i = 0; i < word.length(); i++) {
-
-            char ch = word.charAt(i);
-
-            if (Character.isLowerCase(ch)) {
-                lower[ch - 'a'] = i;
-            } else {
-                if(upper[ch - 'A'] == -1) {
-                upper[ch - 'A'] = i;
-                }
-                
-            }
-        }
-
-        int count = 0;
-        for (int i = 0; i < 26; i++) {
-
-            if (lower[i] != -1 &&
-                upper[i] != -1 &&
-                lower[i] < upper[i]) {
-
-                count++;
-            }
-        }
-
-        return count;
-    }
+		int n = word.length();
+		char[] chars = word.toCharArray();
+		int[] lastLower = new int[26];
+		int[] firstUpper = new int[26];
+		Arrays.fill(lastLower, -1);
+		Arrays.fill(firstUpper, -1);
+		for (int i = 0; i < n; i++) {
+			if (chars[i] >= 'a') {
+				lastLower[chars[i] - 'a'] = i;
+			} else if (firstUpper[chars[i] - 'A'] == -1) {
+				firstUpper[chars[i] - 'A'] = i;
+			}
+		}
+		int ans = 0;
+		for (int i = 0; i < 26; i++) {
+			if (lastLower[i] != -1 && lastLower[i] < firstUpper[i]) {
+				ans++;
+			}
+		}
+		return ans;
+	}
 }
