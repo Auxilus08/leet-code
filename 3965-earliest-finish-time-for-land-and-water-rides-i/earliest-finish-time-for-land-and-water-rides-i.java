3c1a9a36d7352d1779c340bef2bfe787ed721172
Akshat Tiwari <tiwariar_10@rknec.edu>
2026-06-02 20:50:36 +0530

    Time: 3 ms (66.67%) | Memory: 46.7 MB (77.78%) - LeetSync

---
+++ 3965-earliest-finish-time-for-land-and-water-rides-i/earliest-finish-time-for-land-and-water-rides-i.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int earliestFinishTime(int[] landStartTime, int[] landDuration, int[] waterStartTime, int[] waterDuration) {
+        int res = Integer.MAX_VALUE;
+        for(int i = 0; i < landStartTime.length; i++) {
+            for(int j = 0; j < waterStartTime.length; j++) {
+                int land = landStartTime[i] + landDuration[i];
+                int lw = Math.max(land, waterStartTime[j]) + waterDuration[j];
+
+                res = Math.min(res, lw);
+
+
+                int water = waterStartTime[j] + waterDuration[j];
+                int wl = Math.max(water, landStartTime[i]) + landDuration[i];
+
+                res = Math.min(res, wl);
+            }
+
+            
+        }
+        return res;
+
+    }
+}
